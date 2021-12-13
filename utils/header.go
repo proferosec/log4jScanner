26d00b23f1fc140a4c0152e0a69021937929af30
hagarz <hagar@profero.io>
2021-12-13 14:49:50 +0200

    https + go fmt

--- utils/header.go
+++ utils/header.go
@@ -5,14 +5,14 @@ import "github.com/pterm/pterm"
 var Version string = "undefined"
 
 func SetVersion(version string) {
-    Version = version
+	Version = version
 }
 
 func PrintHeader() {
-    // Generate BigLetters
-    pterm.DefaultCenter.WithCenterEachLineSeparately().Println("")
-    s, _ := pterm.DefaultBigText.WithLetters(pterm.NewLettersFromStringWithStyle("Profero", pterm.DefaultBox.TextStyle)).Srender()
-    pterm.DefaultCenter.Println(s) // Print BigLetters with the default CenterPrinter
-    pterm.DefaultCenter.WithCenterEachLineSeparately().Print("log4jScanner")
-    pterm.DefaultCenter.WithCenterEachLineSeparately().Printf("version: %s", Version)
+	// Generate BigLetters
+	pterm.DefaultCenter.WithCenterEachLineSeparately().Println("")
+	s, _ := pterm.DefaultBigText.WithLetters(pterm.NewLettersFromStringWithStyle("Profero", pterm.DefaultBox.TextStyle)).Srender()
+	pterm.DefaultCenter.Println(s) // Print BigLetters with the default CenterPrinter
+	pterm.DefaultCenter.WithCenterEachLineSeparately().Print("log4jScanner")
+	pterm.DefaultCenter.WithCenterEachLineSeparately().Printf("version: %s", Version)
 }
