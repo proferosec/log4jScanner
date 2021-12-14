514b7447222d3c93929f690c15d4c19a30cb6055
hagarz <hagar@profero.io>
2021-12-14 13:16:00 +0200

    cvs output path

--- cmd/scan.go
+++ cmd/scan.go
@@ -180,40 +180,10 @@ func ScanCIDR(ctx context.Context, cidr string, portsFlag string, serverUrl stri
 func PrintResults(resChan chan string) {
 	pterm.Println()
 	pterm.DefaultHeader.WithFullWidth().Println("Results")
-	//csvRecords := [][]string{
-	//	{"type", "ip", "port", "status_code"},
-	//}
+
 	close(resChan)
 	csvRecords := createCsvRecords(resChan)
 	saveCSV(csvRecords)
-	//for res := range resChan {
-	//	csvRes := strings.Split(res, ",")
-	//	msg := fmt.Sprintf("Summary: %s:%s ==> %s", csvRes[1], csvRes[2], csvRes[3])
-	//	pterm.Info.Println(msg)
-	//	log.Info(msg)
-	//	csvRecords = append(csvRecords, csvRes)
-	//}
-	//close(TCPServer.sChan)
-	//for suc := range TCPServer.sChan {
-	//	csvSuc := strings.Split(suc, ",")
-	//	msg := fmt.Sprintf("Summary: Callback from %s:%s", csvSuc[1], csvSuc[2])
-	//	pterm.Info.Println(msg)
-	//	log.Info(msg)
-	//	csvRecords = append(csvRecords, csvSuc)
-	//}
-	//f, err := os.Create("log4jScanner-results.csv")
-	//if err != nil {
-	//	log.Fatal(err)
-	//}
-	//defer f.Close()
-	//w := csv.NewWriter(f)
-	//defer w.Flush()
-	//
-	//for _, record := range csvRecords {
-	//	if err := w.Write(record); err != nil {
-	//		log.Fatal(err)
-	//	}
-	//}
 }
 
 func ScanPorts(ip, server string, ports []int, resChan chan string, wg *sync.WaitGroup) {
